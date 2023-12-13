a481090afca373efc04a0b6b4ed51388ff07928d
Andrew Fahlgren <andrewfahlgren@gmail.com>
2023-12-12 19:35:15 -0600

    updated version to 1.1.0.0

--- Plugin.cs
+++ Plugin.cs
@@ -10,7 +10,7 @@ namespace PotionCraftBookmarkOrganizer
     public class Plugin : BaseUnityPlugin
     {
         public const string PLUGIN_GUID = "com.fahlgorithm.potioncraftbookmarkorganizer";
-        public const string PLUGIN_VERSION = "1.0.5.4";
+        public const string PLUGIN_VERSION = "1.1.0.0";
 
         public static ManualLogSource PluginLogger {get; private set; }
 
